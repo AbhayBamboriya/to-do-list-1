854b071724dea7586399ec4f290ff105cbf043b4
AbhayBamboriya <bamboriya09@gmail.com>
2024-02-27 00:18:40 +0530

    context-api

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,16 +1,24 @@
 import { useState } from "react"
 
-function Todo({todoData,isFinished,changeFinished}){
+function Todo({todoData,isFinished,changeFinished,onDelete,onEdit}){
     const[finished,setFinised]=useState(isFinished)
+    const[isEditing,setIsEditing]=useState(false)
+    const [editText,setEdotText]=useState(todoData)
     return(
         <>
             <input type="checkbox" checked={finished} onChange={(e)=>{
                 setFinised(e.target.checked)
                 changeFinished(e.target.checked)
             }}/>
-            {todoData}
-            <button>Edit</button>
-            <button>Delete</button>
+            {(isEditing)?<input type="text" value={editText} onChange={e=>setEdotText(e.target.value)}/>: todoData}
+
+
+
+            <button onClick={()=>{
+                setIsEditing(!isEditing)
+                onEdit(editText)
+            }}    >{(!isEditing)?'Edit':'Save'}</button>
+            <button onClick={onDelete}>Delete</button>
             <br/>
         </>
     )
