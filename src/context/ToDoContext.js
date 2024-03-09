cbf8a28709e7018e6477546518a5e0c68dac1c3c
AbhayBamboriya <bamboriya09@gmail.com>
2024-03-09 19:36:56 +0530

    Context API

--- src/context/ToDoContext.js
+++ src/context/ToDoContext.js
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 
 export default createContext(null)
-
+// create context 
 // create context will help to create parent component
 // it will be directly accessed by child
